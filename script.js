3867553c62485e56205d04a838ea8079bac8b1bc
Victor Herrera <victor.herrera.sc@itszapopan.edu.mx>
2018-07-25 22:45:05 -0500

    Added files for markup script and style; Added basic math functions

---
+++ script.js
@@ -0,0 +1,14 @@
+function add (number1, number2){
+    return number1+number2;
+}
+function subtract(number1, number2){
+    return number1-number2;
+}
+function multiply(number1, number2){
+    return number1*number2;
+}
+function dividet(number1, number2){
+    if(number2 == 0) return 'Error';
+    return number1/number2;
+}
+
